bc864660d8f7829c881eff98b88c6689b22c1ef6
enockmagara <emm10042@nyu.edu>
2025-04-18 00:56:40 +0400

    test tests

--- tests/unit/test_alerts.py
+++ tests/unit/test_alerts.py
@@ -10,6 +10,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 from app import create_app
 
 
+
 class TestAlerts(unittest.TestCase):
     def setUp(self):
         """Set up test fixtures before each test method is run."""
